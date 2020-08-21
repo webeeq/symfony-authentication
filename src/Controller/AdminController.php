f2f522f88348f9b6b450216ca617d7ee2ce9da5e
Robert Rybiański <40652170+webeeq@users.noreply.github.com>
2020-08-21 13:24:18 +0200

    Update repository files

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,16 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * Require ROLE_ADMIN for *every* controller method in this class.
- *
- * @IsGranted("ROLE_ADMIN")
- */
 class AdminController extends AbstractController
 {
     /**
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,16 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * Require ROLE_USER for *every* controller method in this class.
- *
- * @IsGranted("ROLE_USER")
- */
 class ProfileController extends AbstractController
 {
     /**
